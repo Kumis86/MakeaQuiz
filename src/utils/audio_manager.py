babbb16361c763cefc4cde28d16238ec3a99f6a1
Lia2406 <amaliakhoerina25@gmail.com>
2025-04-09 20:17:51 +0700

    hasil jadi bg music
    music saat masuk(main), music saat mengerjakan soal, notif wrong sound, suara  menekan pilihan benar

---
+++ src/utils/audio_manager.py
@@ -0,0 +1,28 @@
+import pygame
+
+class AudioManager:
+    def __init__(self):
+        pygame.mixer.init()
+        self.current_music = None
+
+    def play_music(self, path, volume=0.3, loop=-1):
+        try:
+            if self.current_music != path:
+                pygame.mixer.music.load(path)
+                pygame.mixer.music.set_volume(volume)
+                pygame.mixer.music.play(loop)
+                self.current_music = path
+        except Exception as e:
+            print(f"[AudioManager] Error playing music: {e}")
+
+    def stop_music(self):
+        pygame.mixer.music.stop()
+        self.current_music = None
+
+    def play_sound_effect(self, path):
+        try:
+            sound = pygame.mixer.Sound(path)
+            sound.set_volume(0.5)
+            sound.play()
+        except Exception as e:
+            print(f"[AudioManager] Error playing sound effect: {e}")
